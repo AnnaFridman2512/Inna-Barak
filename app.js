bed70136ba17401b5ec6820ec53e515ca16dc458
AnnaFridman2512 <annafridman81@gmail.com>
2021-03-19 09:21:48 +0200

    Working borger added

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const navSlide= () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
-
+    const navLinks = document.querySelectorAll('.nav-links li');//get each nav link by it's own
+    
     burger.addEventListener('click', () => {
+        //Toggle nav
         nav.classList.toggle('nav-active');
+
+        //Animate links
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+               link.style.animation='';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.7}s`;//delay
+            }
+        });
+        //Burger Animation
+    burger.classList.toggle('toggle');
     });
 }
 
