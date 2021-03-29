e7241ec973490e5d36b2b03e4956bf3ee4bc4555
AnnaFridman2512 <annafridman81@gmail.com>
2021-03-29 14:01:48 +0300

    Second carousel-slide added

--- app.js
+++ app.js
@@ -66,3 +66,49 @@ carouselSlide.addEventListener('transitionend', ()=>{
      carouselSlide.style.transform = 'translateX(' + (size * counter) + 'px)' ;
      }    
   });
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const carouselSlide2 = document.querySelector('.carousel-slide2');    
+const carouselImages2 = document.querySelectorAll('.carousel-slide2 img');
+
+//Buttons2 
+const prevBtn2 = document.querySelector('.prevBtn2');
+const nextBtn2 = document.querySelector('.nextBtn2');
+
+//Counter2
+let counter2 = 1;
+const size2 = carouselImages2[0].clientWidth;
+carouselSlide2.style.transform = 'translateX(' + (size2 * counter2) + 'px)' ;
+
+//Button Listeners2
+nextBtn2.addEventListener('click', () => {
+    if(counter2 >= carouselImages2.length -1) return;
+    carouselSlide2.style.transition = "transform 0.4s ease-in-out";
+    counter2++;
+carouselSlide2.style.transform = 'translateX('+ (size2 * counter2) + 'px)' ;
+});
+
+
+prevBtn2.addEventListener('click', () => {
+    if(counter2 <= 0) return;
+    carouselSlide2.style.transition = "transform 0.4s ease-in-out";
+    counter2--;
+    carouselSlide2.style.transform = 'translateX('+ (size2 * counter2) + 'px)' ;
+});
+
+carouselSlide2.addEventListener('transitionend', ()=>{
+  if(carouselImages2[counter2].className === 'lastClone2'){
+       carouselSlide2.style.transition = "none";
+       counter2 = carouselImages2.length - 2 ;
+   carouselSlide2.style.transform = 'translateX(' + (size2 * counter2) + 'px)' ;
+   }    
+});
+
+
+carouselSlide2.addEventListener('transitionend', ()=>{
+    if(carouselImages2[counter2].className === 'firstClone2'){
+         carouselSlide2.style.transition = "none";
+         counter2 = carouselImages2.length - counter2;
+     carouselSlide2.style.transform = 'translateX(' + (size2 * counter2) + 'px)' ;
+     }    
+  });
