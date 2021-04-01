51a17bb66e9ecf1094aa424b8f708c3c1776d9f3
AnnaFridman2512 <annafridman81@gmail.com>
2021-04-01 14:17:44 +0300

    nav toggle added

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-const navSlide= () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');//get each nav link by it's own
-    
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li');//get each nav link by it's own
+
+
+nav.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+        //Animate links
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+               link.style.animation='';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.7}s`;//delay
+            }
+        });
+         //Burger Animation
+        burger.classList.toggle('toggle');
+});
+
+const navSlide= () => {    
     burger.addEventListener('click', () => {
         //Toggle nav
         nav.classList.toggle('nav-active');
@@ -18,9 +33,14 @@ const navSlide= () => {
         //Burger Animation
     burger.classList.toggle('toggle');
     });
-}
+   
+} 
 
-    navSlide();
+
+
+navSlide(); 
+
+
 
 
 
